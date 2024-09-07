b99bb176718f0183626d63a3e422a043035d0642
YounesAmazzalOriginal <younesamazzalwork@gmail.com>
2024-09-07 22:48:53 +0100

    Add files via upload

---
+++ Seo WEBSITE/NavBar.js
@@ -0,0 +1,17 @@
+// .Mobile_Nav {
+//     display: flex;
+//     background-color: black;
+//     position: relative;
+
+//     display: none;
+//   }
+
+var Mobile_Nav = document.querySelector(".Mobile_Nav");
+
+function open_nav_function() {
+  Mobile_Nav.style.display = "flex";
+}
+
+function close_nav_function() {
+  Mobile_Nav.style.display = "none";
+}
